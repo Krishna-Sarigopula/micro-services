dc54e97092e71de824fd3073e5c73e0c537da0e6
Krishna Sarigopula <krishna.sarigopula@gmail.com>
2022-09-17 01:05:34 +0530

    microservices

---
+++ src/platformService/Models/Platform.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatoformService.Models
+{
+    public class Platform
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string Cost { get; set; }
+    }
+}
